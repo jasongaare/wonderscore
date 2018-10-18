478f63d83bbb1baea88e15b45329d55ed81a0f83
Jason Gaare <jason.d.gaare@gmail.com>
2018-10-18 00:33:39 -0500

    added rows for all players, header, footer

--- src/App.js
+++ src/App.js
@@ -1,63 +1,60 @@
 import React, { Component } from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import {
+	Dimensions,
+	findNodeHandle,
+	Keyboard,
+	ScrollView,
+	StyleSheet,
+	Switch,
+	Text,
+	TextInput,
+	View
+} from "react-native";
 import isEqual from "lodash.isequal";
+import { columns, columnColors, summableColumns } from "./columns";
 
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+const CONTAINER_PADDING = 16;
+const ROW_HEIGHT = SCREEN_HEIGHT / 12;
 const NUM_OF_PLAYERS = 7;
+
 const baseInputStyle = {
 	borderColor: "black",
 	borderWidth: 1,
 	color: "black",
-	fontSize: 20,
+	fontSize: 18,
 	height: "100%",
 	textAlign: "center"
 };
 
-const columns = [
-	"name",
-	"military",
-	"treasury",
-	"wonders",
-	"civilian",
-	"commercial",
-	"guilds",
-	"science",
-	"totalpoints"
-];
+const baseHeaderTextStyle = {
+	color: "white",
+	textAlign: "center",
+	borderColor: "black",
+	borderWidth: 1,
+	flex: 1
+};
 
-const summableColumns = [
-	"military",
-	"treasury",
-	"wonders",
-	"civilian",
-	"commercial",
-	"guilds",
-	"science"
-];
-
-const columnColors = [
-	"white",
-	"indianred",
-	"lemonchiffon",
-	"wheat",
-	"royalblue",
-	"orange",
-	"blueviolet",
-	"mediumseagreen",
-	"white"
-];
-
-export default class App extends Component {
+class App extends Component {
 	constructor(props) {
 		super(props);
 
+		this.scrollview = React.createRef();
+		const didHideListener = Keyboard.addListener("keyboardDidHide", () => {
+			this.scrollview.current.scrollTo({ y: 0, animated: true });
+		});
+
 		this.state = {
+			didHideListener,
 			inputValues: {},
-			inputRefs: {}
+			inputRefs: {},
+			showTotals: true
 		};
 	}
 
 	componentDidMount() {
-		this.createValues(NUM_OF_PLAYERS);
+		this.initializeRefs();
+		this.clearValues();
 	}
 
 	componentDidUpdate(prevProps, prevState) {
@@ -68,30 +65,59 @@ export default class App extends Component {
 		}
 	}
 
-	createValues = playerNum => {
-		if (playerNum === 0) {
-			return;
+	componentWillUnmount() {
+		const { didHideListener } = this.state;
+		Keyboard.removeSubscription(didHideListener);
+	}
+
+	scrollToFocusedInput = ref => {
+		const node = findNodeHandle(ref);
+		setTimeout(() => {
+			if (
+				this.scrollview &&
+				this.scrollview.current &&
+				this.scrollview.current.getScrollResponder
+			) {
+				const scrollResponder = this.scrollview.current.getScrollResponder();
+				scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
+					node,
+					70,
+					true
+				);
+			}
+		}, 100);
+	};
+
+	initializeRefs = () => {
+		const refs = {};
+
+		for (let i = 1; i <= NUM_OF_PLAYERS; i++) {
+			columns.forEach(column => {
+				const key = `p${i}-${column}`;
+
+				refs[key] = React.createRef();
+			});
 		}
 
-		const { inputValues, inputRefs } = this.state;
-		const valuesForPlayer = {};
-		const refsForPlayer = {};
-
-		columns.forEach(column => {
-			const key = `p${playerNum}-${column}`;
-			valuesForPlayer[key] = "";
-			refsForPlayer[key] = React.createRef();
+		this.setState({
+			inputRefs: Object.assign({}, refs)
 		});
+	};
 
-		this.setState(
-			{
-				inputValues: Object.assign({}, inputValues, valuesForPlayer),
-				inputRefs: Object.assign({}, inputRefs, refsForPlayer)
-			},
-			() => {
-				this.createValues(playerNum - 1);
-			}
-		);
+	clearValues = () => {
+		const { inputValues } = this.state;
+		const nexInputValues = {};
+
+		for (let i = 1; i <= NUM_OF_PLAYERS; i++) {
+			summableColumns.forEach(column => {
+				const key = `p${i}-${column}`;
+				nexInputValues[key] = "";
+			});
+		}
+
+		this.setState({
+			inputValues: Object.assign({}, inputValues, nexInputValues)
+		});
 	};
 
 	updateTotals = () => {
@@ -115,27 +141,40 @@ export default class App extends Component {
 		});
 	};
 
-	render() {
-		const rowInputs1 = [];
+	createRowForPlayer = num => {
+		const rowInputs = [];
 		const { inputValues, inputRefs } = this.state;
 
 		columns.forEach((column, index) => {
-			const key = `p1-${column}`;
+			const key = `p${num}-${column}`;
 			const isName = column === "name";
 			const isTotal = column === "totalpoints";
 
-			rowInputs1.push(
+			// handle special case for totalpoints column value
+			let inputValue = inputValues[key];
+			if (isTotal) {
+				const totalIntValue = parseInt(inputValue);
+				const { showTotals } = this.state;
+
+				if (!showTotals || totalIntValue === 0) {
+					inputValue = "";
+				}
+			}
+
+			rowInputs.push(
 				<TextInput
 					key={key}
 					ref={inputRefs[key]}
 					editable={!isTotal}
 					keyboardType={isName ? "default" : "numbers-and-punctuation"}
+					keyboardShouldPersistTaps="always"
+					autoCorrect={false}
 					style={{
 						...baseInputStyle,
 						backgroundColor: columnColors[index],
 						flex: isName ? 3 : 1
 					}}
-					value={inputValues[key]}
+					value={inputValue}
 					onChangeText={text => {
 						const newValue = {};
 						newValue[key] = text;
@@ -143,19 +182,127 @@ export default class App extends Component {
 							inputValues: Object.assign({}, inputValues, newValue)
 						});
 					}}
+					onFocus={() => this.scrollToFocusedInput(inputRefs[key].current)}
 					onSubmitEditing={() => {
 						const nextIndex = index + 1;
 						if (nextIndex < columns.length - 1) {
-							inputRefs[`p1-${columns[nextIndex]}`].current.focus();
+							inputRefs[`p${num}-${columns[nextIndex]}`].current.focus();
 						}
 					}}
 				/>
 			);
 		});
 
+		return rowInputs;
+	};
+
+	renderScorecardHeader = () => {
+		return (
+			<View
+				style={{
+					flexDirection: "row",
+					height: ROW_HEIGHT,
+					backgroundColor: "black",
+					width: "100%",
+					alignItems: "center"
+				}}
+			>
+				<Text style={{ ...baseHeaderTextStyle, flex: 3 }}>Player Name</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>A</Text>
+				<Text style={{ ...baseHeaderTextStyle }}>Score</Text>
+			</View>
+		);
+	};
+
+	renderScorecardFooter = () => {
+		const { showTotals } = this.state;
+		return (
+			<View
+				style={{
+					flexDirection: "row",
+					height: ROW_HEIGHT,
+					backgroundColor: "black",
+					width: "100%",
+					alignItems: "center",
+					paddingHorizontal: 4
+				}}
+			>
+				<Text
+					style={{
+						...baseHeaderTextStyle,
+						color: "khaki",
+						textAlign: "left"
+					}}
+					onPress={() => this.clearValues()}
+				>
+					{"Clear All Scores"}
+				</Text>
+
+				<View style={{ flexDirection: "row", alignItems: "center", flex: 1 }}>
+					<Text
+						style={{ ...baseHeaderTextStyle, flex: 0, marginHorizontal: 8 }}
+					>
+						{"Total Scores"}
+					</Text>
+					<Switch
+						style={{ transform: [{ scale: 0.8 }] }}
+						value={showTotals}
+						onValueChange={val => {
+							this.setState({ showTotals: val });
+						}}
+					/>
+				</View>
+
+				<Text
+					style={{
+						...baseHeaderTextStyle,
+						color: "khaki",
+						flex: 2,
+						textAlign: "right"
+					}}
+				>
+					{"Scientific Score Calculator"}
+				</Text>
+			</View>
+		);
+	};
+
+	render() {
+		const inputRows = [];
+		for (let i = 1; i <= NUM_OF_PLAYERS; i++) {
+			inputRows.push(
+				<View
+					key={`p${i}-inputRow`}
+					style={{ flexDirection: "row", height: ROW_HEIGHT }}
+				>
+					{this.createRowForPlayer(i)}
+				</View>
+			);
+		}
+
 		return (
 			<View style={styles.container}>
-				<View style={{ flexDirection: "row", height: 32 }}>{rowInputs1}</View>
+				<ScrollView
+					ref={this.scrollview}
+					style={{ width: "100%" }}
+					contentContainerStyle={{
+						marginTop:
+							(SCREEN_HEIGHT - 2 * CONTAINER_PADDING - 9 * ROW_HEIGHT) / 2
+					}}
+					showsVerticalScrollIndicator={false}
+					keyboardDismissMode="on-drag"
+					keyboardShouldPersistTaps="handled"
+				>
+					{this.renderScorecardHeader()}
+					{inputRows}
+					{this.renderScorecardFooter()}
+				</ScrollView>
 			</View>
 		);
 	}
@@ -164,9 +311,11 @@ export default class App extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "flex-start",
+		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "khaki",
-		padding: 16
+		padding: CONTAINER_PADDING
 	}
 });
+
+export default App;
---
+++ src/columns.js
@@ -0,0 +1,35 @@
+const columns = [
+	"name",
+	"military",
+	"treasury",
+	"wonders",
+	"civilian",
+	"commercial",
+	"guilds",
+	"science",
+	"totalpoints"
+];
+
+const summableColumns = [
+	"military",
+	"treasury",
+	"wonders",
+	"civilian",
+	"commercial",
+	"guilds",
+	"science"
+];
+
+const columnColors = [
+	"white",
+	"indianred",
+	"lemonchiffon",
+	"wheat",
+	"royalblue",
+	"orange",
+	"blueviolet",
+	"mediumseagreen",
+	"white"
+];
+
+export { columns, summableColumns, columnColors };
